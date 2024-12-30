ce989d636f643b9efb43de44dc046a648b422214
Elitex07 <jain.yashu2211@gmail.com>
2024-12-30 12:31:09 +0530

    feat: connect to database

---
+++ src/lib/mongo.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const MONGO_URI = process.env.mongo;
+
+if (!MONGO_URI) throw new Error('Please add your Mongo URI to .env.local');
+
+let isConnected = false; // Track connection status.
+
+export const connectToDatabase = async () => {
+  if (isConnected) return;
+
+  try {
+    const db = await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = db.connections[0].readyState;
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB', error);
+    throw error;
+  }
+};
