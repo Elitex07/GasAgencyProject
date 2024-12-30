a43db290c05596823ffc6703a35d71d0a31929cf
Elitex07 <jain.yashu2211@gmail.com>
2024-12-30 13:02:52 +0530

    feat: booking model

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -7,17 +7,17 @@ if (!MONGO_URI) throw new Error('Please add your Mongo URI to .env.local');
 let isConnected = false; // Track connection status.
 
 export const connectToDatabase = async () => {
-  if (isConnected) return;
+    if (isConnected) return;
 
-  try {
-    const db = await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    isConnected = db.connections[0].readyState;
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('Error connecting to MongoDB', error);
-    throw error;
-  }
+    try {
+        const db = await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        isConnected = db.connections[0].readyState;
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB', error);
+        throw error;
+    }
 };
